23b775fbfe7e8cf185b477524bcae7c0cdd74d7e
bm12 <mahmud1997babaev@mail.ru>
2017-10-27 23:48:55 +0300

    toogle class 'selected-dialog'

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-$('.dialog__body').customScroll({
-	offsetTop: 1,
-	offsetBottom: 1
-});
-$('.msg-history__chat-wrap').customScroll({
-	offsetTop: 1,
-	offsetBottom: 1
-});
-$(window).load(function() {
-	$('.dialog__body, .msg-history__chat-wrap').customScroll();
+$(document).ready(function() {
+	$('.dialog__body-item').on('click', function(e){
+		$('.dialog__body-item.selected-dialog').removeClass('selected-dialog');
+		$(this).addClass('selected-dialog');
+	});
+	
+	$('.dialog__body').customScroll({
+		offsetTop: 1,
+		offsetBottom: 1
+	});
+	$('.msg-history__chat-wrap').customScroll({
+		offsetTop: 1,
+		offsetBottom: 1
+	});
+	$(window).load(function() {
+		$('.dialog__body, .msg-history__chat-wrap').customScroll();
+	});
 });
